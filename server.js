573b49483455a141dd69a9eed9f04cc3c0c3e4fd
Minsoo <zlemzlem5656@naver.com>
2020-06-28 13:56:53 +0900

    0.1.0 deploy test

--- server.js
+++ server.js
@@ -8,9 +8,9 @@ const bodyParser = require("body-parser");
 const bucketListItemRoutes = require("./routes/api/bucketListItems");
 const path = require("path");
 
-app.use(cors());
-app.use(morgan("tiny"));
-app.use(bodyParser.json());
+app.use(cors()); // CORS 미들웨어
+app.use(morgan("tiny")); // 요청에 대한 log를 남겨주는 미들웨어
+app.use(bodyParser.json()); // POST 요청에 대한 body 값을 받아올 수 있게 해주는 미들웨어
 
 mongoose
   .connect(mongoUri, {
@@ -24,6 +24,7 @@ mongoose
 
 app.use("/api/bucketListItems", bucketListItemRoutes);
 
+// 배포시 build 된 파일들을 돌리기 위한 코드
 if (process.env.NODE_ENV === "production") {
   app.use(express.static("client/dist"));
   app.get("*", (req, res) => {

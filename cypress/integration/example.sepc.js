01c9cec98e4c695ae4fc96f3ea40acbf3a52fcf9
Minsoo <zlemzlem5656@naver.com>
2020-08-07 09:46:16 +0900

    안쓰는 저장소 초기화

--- cypress/integration/example.sepc.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-context("CRUD", () => {
-  it("CREATE", () => {
-    cy.visit("http://mevn-bucket-list-app-ms.herokuapp.com");
-    cy.get("input.input").click().type("test type");
-    cy.get(".button.is-info").click();
-  });
-});
